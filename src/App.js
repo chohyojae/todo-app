fad5dcdf592fbf7de75b9f3a3d8c00d510f5421c
hjcho <hjcho@seegene.com>
2021-06-21 15:14:46 +0900

    TodoList components and stylings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  );
 };
 
 export default App;
