d3e1e0873c2aa5447e509ac54ee27403fcc7accd
hjcho <hjcho@seegene.com>
2021-06-21 16:33:36 +0900

    useState

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todoItem) => {
+        console.log(todoItem);
+        return <TodoListItem todo={todoItem} key={todoItem.id} />;
+      })}
     </div>
   );
 };
